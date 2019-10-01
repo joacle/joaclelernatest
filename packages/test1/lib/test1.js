ca1f1c3f1cce416002d0a7ab66a0ad57c9f77c46
joacle <joaquinfs@hotmail.com>
2019-10-01 15:13:13 +0200

    remove comments

--- packages/test1/lib/test1.js
+++ packages/test1/lib/test1.js
@@ -4,9 +4,4 @@ module.exports = test1;
 
 function test1() {
     // TODO asdasd asd asdasdas  as das das asd as
-    // lets seee!
-    // change!
-    // change 2
-    // change 3
-    // change 4
 }
